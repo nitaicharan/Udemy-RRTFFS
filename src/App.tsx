6c897294bbb6f762d56351e521ceba8b914c5fe8
Nitai Charan <nitaicharan@gmail.com>
2021-03-23 18:05:49 -0300

    17. Métodos private, public y protected

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        <span>Lala!</span>
-      </p>
+      <Header/>
+      <Intro/>
+      <Intro/>
     </div>
   );
 }
---
+++ src/components/Intro.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Intro extends Component {
+    render() {
+        return (
+            <p className="App-intro">
+                <span>Lala!</span>
+            </p>
+        )
+    }
+}
+
+export default Intro
