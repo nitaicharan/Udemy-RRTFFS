97cc739c431b03f45aa3ae0aa81dae9baea58778
Nitai Charan <nitaicharan@gmail.com>
2021-04-01 00:00:00 -0300

    53. Montando la ruta y creando el contenedor

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Login from './containers/Auth/Login';
 import Register from './containers/Auth/Register';
 import NewsFeed from './containers/NewsFeed';
+import Profile from './containers/Profile';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/register" component={Register} />
       <Route path="/app/" component={Navbar} />
       <Route path="/app/newsfeed" component={NewsFeed} />
+      <Route path="/app/Profile" component={Profile} />
       <Route exact path="/" component={Login} />
     </div>
   );
---
+++ src/containers/Profile/index.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const style = {
+    container: {
+        padding: '15px',
+    },
+};
+export default class index extends Component {
+    render() {
+        return (
+            <div style={style.container}>
+                lala
+            </div>
+        )
+    }
+}
