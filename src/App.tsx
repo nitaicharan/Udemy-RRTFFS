6bf9a4ef74b9d60d99307ab7d43cea2a817e236e
Nitai Charan <nitaicharan@gmail.com>
2021-04-02 00:00:00 -0300

    52. Navbar, creamos nuestro componente de navegación

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 import Login from './containers/Auth/Login';
 import Register from './containers/Auth/Register';
 import NewsFeed from './containers/NewsFeed';
@@ -8,11 +9,10 @@ import NewsFeed from './containers/NewsFeed';
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/app/newsfeed" component={NewsFeed} />
-      </Switch>
+      <Route path="/register" component={Register} />
+      <Route path="/app/" component={Navbar} />
+      <Route path="/app/newsfeed" component={NewsFeed} />
+      <Route exact path="/" component={Login} />
     </div>
   );
 }
