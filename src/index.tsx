508ee6f821b07af8cdcc8e8113a5210c5e32318a
Nitai Charan <nitaicharan@gmail.com>
2021-04-05 19:22:43 -0300

    57. Agregando redux y thunks

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
+import * as reducers from "./ducks";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(combineReducers({
+  ...reducers
+}), applyMiddleware(thunk));
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
