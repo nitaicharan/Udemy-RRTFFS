6e5fc5387c77a3ecc8b3f36429c79fa4666164ab
Nitai Charan <nitaicharan@gmail.com>
2021-04-05 18:28:21 -0300

    55. Listado de imágenes del usuario

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
 import Button from '../../components/Button';
+import Card from '../../components/Card';
 import ProfileImg from '../../components/ProfileImg';
 
 const style = {
     container: {
         padding: '15px',
+    },
+    row: {
         display: 'flex',
         justifyContent: 'space-between',
+        marginBottom: '10px'
     },
 };
 export default class index extends Component {
     render() {
         return (
             <div style={style.container}>
-                <ProfileImg />
-                <Button>Add</Button>
+                <div style={style.row}>
+                    <ProfileImg />
+                    <Button>Add</Button>
+                </div>
+                <div style={style.row}>
+                    <Card><img src="https://placekitten.com/100/100" /></Card>
+                    <Card><img src="https://placekitten.com/100/100" /></Card>
+                    <Card><img src="https://placekitten.com/100/100" /></Card>
+                </div>
+                <div style={style.row}>
+                    <Card><img src="https://placekitten.com/100/100" /></Card>
+                    <Card><img src="https://placekitten.com/100/100" /></Card>
+                    <Card><img src="https://placekitten.com/100/100" /></Card>
+                </div>
             </div>
         )
     }
